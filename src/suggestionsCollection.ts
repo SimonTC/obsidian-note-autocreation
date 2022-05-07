39750e4ed3f6bc0fcd7aee6514d9261712ada86a
Simon Clement <simon@simontc.com>
2022-05-08 02:05:04 +0300

    Create function to check if we should trigger a suggestion

--- src/suggestionsCollection.ts
+++ src/suggestionsCollection.ts
@@ -27,6 +27,45 @@ export class Suggestion{
 	}
 }
 
+export type DocumentLocation = {
+	line: number,
+	ch: number
+}
+
+export type SuggestionTrigger = {
+	start: DocumentLocation,
+	end: DocumentLocation,
+	query: string
+}
+
+const regex = new RegExp(/(?:^| )@(?!.*]])(.*)/, "d"); // d flag is necessary to get the indices of the groups
+
+export function extractSuggestionTrigger(lineText: string, cursorPosition: DocumentLocation): SuggestionTrigger {
+	const triggerSymbolIndex = lineText.indexOf('@')
+	if (lineText.length === 0 || cursorPosition.ch === 0 || triggerSymbolIndex == -1){
+		return null;
+	}
+
+	const textToSearch = lineText.slice(0, cursorPosition.ch)
+	const match = regex.exec(textToSearch)
+
+	if (!match){
+		return null;
+	}
+
+	const myMatch: any = match; // Need to convert to any to get access to the indices somewhat easily
+	const query = match[1] // Contains the text of the first found group
+	const groupIndices = myMatch.indices
+	const queryStartIndex = Number(groupIndices[1][0])
+	const queryEndIndex = Number(groupIndices[1][1])
+
+	return {
+		start: { line: cursorPosition.line, ch: queryStartIndex },
+		end: { line: cursorPosition.line, ch: queryEndIndex  },
+		query: query
+	}
+}
+
 export class SuggestionCollector{
 	private metadata: IMetadataCollection;
 
--- tests/plugin.test.ts
+++ tests/plugin.test.ts
@@ -1,4 +1,9 @@
-import {IMetadataCollection, Suggestion, SuggestionCollector} from "../src/suggestionsCollection";
+import {
+	extractSuggestionTrigger,
+	IMetadataCollection,
+	Suggestion,
+	SuggestionCollector
+} from "../src/suggestionsCollection";
 
 
 describe('the list of suggestions', function () {
@@ -185,6 +190,38 @@ describe('when the suggestion for a link to a non-existing file is accepted', fu
 	})
 
 	test('only the suggestion name is shown in the document', () => {
+
+	})
+});
+
+describe('a suggestion trigger', function () {
+	it.each([
+		{inputLine: "", cursorPosition:{line: 1, ch: 5}, expectedTrigger: null, description: 'line is empty'},
+		{inputLine: "@Some line", cursorPosition:{line: 1, ch: 0}, expectedTrigger: null, description: 'cursor is at start of line'},
+		{inputLine: "Some line", cursorPosition:{line: 1, ch: 3}, expectedTrigger: null, description: 'there is no trigger symbol on the line'},
+		{inputLine: "@", cursorPosition:{line: 1, ch: 1}, expectedTrigger: {startIndex: 1, endIndex: 1, query: ''}, description: 'line only has trigger symbol'},
+		{inputLine: "My line @", cursorPosition:{line: 1, ch: 9}, expectedTrigger: {startIndex: 9, endIndex: 9, query: ''}, description: 'trigger symbol is at the end of the line'},
+		{inputLine: "This is @James Jameson", cursorPosition:{line: 1, ch: 22}, expectedTrigger: {startIndex: 9, endIndex: 22, query: 'James Jameson'}, description: 'trigger symbol is in middle of line'},
+		{inputLine: "My email is @James@testing.com", cursorPosition:{line: 1, ch: 30}, expectedTrigger: {startIndex: 13, endIndex: 30, query: 'James@testing.com'}, description: 'trigger symbol is followed by another trigger symbol'},
+		{inputLine: "My [[name]] is @John", cursorPosition:{line: 1, ch: 20}, expectedTrigger: {startIndex: 16, endIndex: 20, query: 'John'}, description: 'trigger symbol is after obsidian link'},
+		{inputLine: "My [[name @test]] is @John", cursorPosition:{line: 1, ch: 26}, expectedTrigger: {startIndex: 22, endIndex: 26, query: 'John'}, description: 'trigger symbol is after obsidian link with trigger symbol'},
+		{inputLine: "My email is [[James@testing.com]]", cursorPosition:{line: 1, ch: 33}, expectedTrigger: null, description: 'trigger symbol is inside obsidian link'},
+		{inputLine: "My @name is [[James]]", cursorPosition:{line: 1, ch: 8}, expectedTrigger: {startIndex: 4, endIndex: 8, query: 'name'}, description: 'trigger symbol and cursor is before obsidian link'},
+		{inputLine: "My email is James@testing.com", cursorPosition:{line: 1, ch: 29}, expectedTrigger: null, description: 'trigger symbol is in the middle of a word'},
+		{inputLine: "My email is James@", cursorPosition:{line: 1, ch: 18}, expectedTrigger: null, description: 'trigger symbol is in the end of a word'},
+	])('has trigger $expectedTrigger when $description', (testData) => {
+		const observedTrigger = extractSuggestionTrigger(testData.inputLine, testData.cursorPosition);
+
+		if (testData.expectedTrigger === null){
+			expect(observedTrigger).toBeNull()
+			return
+		}
+
+		expect(observedTrigger).not.toBeNull()
+
+		expect(observedTrigger.end).toEqual({line: testData.cursorPosition.line, ch: testData.expectedTrigger.endIndex});
+		expect(observedTrigger.start).toEqual({line: testData.cursorPosition.line, ch: testData.expectedTrigger.startIndex});
+		expect(observedTrigger.query).toEqual(testData.expectedTrigger.query);
 
 	})
 });
