534175fccf83e0f59c7848026c27209c5d0f01ff
Simon Clement <simon@simontc.com>
2022-05-09 19:01:14 +0300

    Begin on interface for interacting with the file system

--- src/ObsidianInterfaces.ts
+++ src/ObsidianInterfaces.ts
@@ -4,3 +4,14 @@
 export interface IMetadataCollection{
 	getUnresolvedLinks():  Record<string, Record<string, number>>
 }
+
+/**
+ * Contains methods for interacting and getting information about the file system through Obsidian.
+ */
+export interface IFileSystem{
+	/**
+	 * Returns true if a note exist with the given path
+	 * @param notePath the path to check
+	 */
+	doesNoteExist(notePath: string): boolean
+}
