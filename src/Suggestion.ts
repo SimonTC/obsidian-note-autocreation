e1b3ad740954fe2b9ea08ae2a03cb0cd6533c4ae
Simon Clement <simon@simontc.com>
2022-05-12 21:53:51 +0300

    Add support for parsing alias

--- src/Suggestion.ts
+++ src/Suggestion.ts
@@ -3,11 +3,14 @@ export class Suggestion {
 	readonly VaultPath: string;
 	readonly Title: string;
 	readonly FolderPath: string
+	readonly Alias: string
 
 	constructor(vaultPath: string) {
 		const fullPath = vaultPath.trim()
 		this.VaultPath = fullPath;
-		this.Title = this.extractTitle(fullPath);
+		let {title, alias} = this.extractTitleAndAlias(fullPath);
+		this.Title = title
+		this.Alias = alias
 		this.FolderPath = this.extractFolderPath(fullPath);
 	}
 
@@ -18,12 +21,14 @@ export class Suggestion {
 			: vaultPath.slice(0, fileNameStartsAt);
 	}
 
-	private extractTitle(vaultPath: string): string {
+	private extractTitleAndAlias(vaultPath: string): {title: string, alias: string} {
 		const fileNameWithPossibleExtension = vaultPath.split('/').pop();
 		const extensionStartsAt = fileNameWithPossibleExtension.lastIndexOf('.')
-
-		return extensionStartsAt === -1
+		const titleWithPossibleAlias = extensionStartsAt === -1
 			? fileNameWithPossibleExtension
 			: fileNameWithPossibleExtension.slice(0, extensionStartsAt);
+		let [title, alias] = titleWithPossibleAlias.split('|')
+		alias = alias?.length === 0 ? undefined : alias
+		return {title, alias};
 	}
 }
--- tests/suggestion.test.ts
+++ tests/suggestion.test.ts
@@ -3,6 +3,7 @@ import {Suggestion} from "../src/Suggestion";
 describe('a single suggestion', function () {
 	it.each([
 		{vaultPath: 'folder1/folder2/mynote.md', expectedFolderPath: 'folder1/folder2'},
+		{vaultPath: 'folder1/folder2/note 1|other name', expectedFolderPath: 'folder1/folder2'},
 		{vaultPath: 'folder2/mynote.md', expectedFolderPath: 'folder2'},
 		{vaultPath: 'mynote.md', expectedFolderPath: ''},
 	])('does not contain file name in folder path when vault path is $vaultPath', ({vaultPath, expectedFolderPath}) => {
@@ -15,6 +16,8 @@ describe('a single suggestion', function () {
 		{vaultPath: 'folder1/folder2/mynote.md', expectedTitle: 'mynote'}, // normal link
 		{vaultPath: 'folder1/folder2/some note', expectedTitle: 'some note'}, // link without extension
 		{vaultPath: 'folder1/folder2/with some extension.exe', expectedTitle: 'with some extension'}, // wrong extension given
+		{vaultPath: 'My note|With another name', expectedTitle: 'My note'}, // alias given
+		{vaultPath: 'My note.md|With another name', expectedTitle: 'My note'}, // alias given with extension in path
 	])('uses title $expectedTitle when vault path is $vaultPath', ({vaultPath, expectedTitle}) => {
 		const suggestion = new Suggestion(vaultPath);
 
@@ -26,6 +29,7 @@ describe('a single suggestion', function () {
 		{vaultPath: 'folder1/'},
 		{vaultPath: ''},
 		{vaultPath: '/'},
+		{vaultPath: '/|some non empty name'},
 	])('has empty title when vault path is $vaultPath', ({vaultPath}) => {
 		const suggestion = new Suggestion(vaultPath);
 
@@ -47,9 +51,20 @@ describe('a single suggestion', function () {
 		{vaultPath: 'folder1/folder2/some note'},
 		{vaultPath: 'reading/books/short-stories/how I Won.md'},
 		{vaultPath: 'note.md'},
+		{vaultPath: 'folder1/myNote|With another name'},
 	])('stores $vaultPath as vault path', ({vaultPath}) => {
 		const suggestion = new Suggestion(vaultPath);
 
 		expect(suggestion.VaultPath).toBe(vaultPath)
+	})
+
+	it.each([
+		{vaultPath: 'folder1/myNote|With another name', expectedAlias: 'With another name'},
+		{vaultPath: 'My nice note|With another name', expectedAlias: 'With another name'},
+		{vaultPath: 'My nice note|', expectedAlias: undefined},
+		{vaultPath: 'My nice note', expectedAlias: undefined},
+	])('uses alias $expectedAlias when vault path $vaultPath is given', ({vaultPath, expectedAlias}) => {
+		const suggestion = new Suggestion(vaultPath);
+		expect(suggestion.Alias).toBe(expectedAlias)
 	})
 });
