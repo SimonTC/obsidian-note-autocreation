fd4102406ff667947c0438cd8c0b4fb51dd82e57
Simon Clement <simon@simontc.com>
2022-05-13 17:06:18 +0300

    Show warning if problematic symbol is used as trigger

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ import {
 	EditorSuggestTriggerInfo,
 	Plugin,
 	PluginSettingTab,
-	Setting,
+	Setting, TextComponent,
 	TFile
 } from 'obsidian';
 
@@ -123,6 +123,10 @@ class LinkSuggestor extends EditorSuggest<string>{
 class SettingTab extends PluginSettingTab {
 	plugin: NoteAutoCreator;
 
+	// Problematic symbols are based on this table from the markdown guide:
+	// https://www.markdownguide.org/basic-syntax/#characters-you-can-escape
+	private readonly problematicSymbols = ["\\", "`", "*", "_", "{", "}", "[", "]", "<", ">", "(", ")", "#", "+", "-", ".", "!", "|"]
+
 	constructor(app: App, plugin: NoteAutoCreator) {
 		super(app, plugin);
 		this.plugin = plugin;
@@ -130,19 +134,40 @@ class SettingTab extends PluginSettingTab {
 
 	display(): void {
 		const {containerEl} = this;
-
 		containerEl.empty();
 
-		containerEl.createEl('h2', {text: 'Settings for the Note Auto Creator plugin'});
+		const trigger = new Setting(containerEl)
+			.setName('Trigger for link insertion')
+			.setDesc('The text string that will trigger link selection.')
+			.setTooltip(
+				'The string can contain multiple symbols such as @@ ' +
+				'Avoid using characters / strings you often use while writing or that might be used by Obsidian or by other plugins to trigger actions. ' +
+				'Some examples of strings to avoid: "[", "|", "#"');
+		trigger.addText(component => component
+			.setPlaceholder('@')
+			.setValue(this.plugin.settings.triggerSymbol)
+			.onChange(async (value) => {
+				this.removeValidationWarning(component, trigger);
+				this.warnIfTriggerIsProblematic(value, trigger, component);
+				this.plugin.settings.triggerSymbol = value;
+				await this.plugin.saveSettings();
+			}));
+	}
 
-		new Setting(containerEl)
-			.setName('Trigger icon for link insertion')
-			.setDesc('The icon that will trigger link selection. @ by default.')
-			.addText(component => component
-				.setValue(this.plugin.settings.triggerSymbol)
-				.onChange(async (value) => {
-					this.plugin.settings.triggerSymbol = value;
-					await this.plugin.saveSettings();
-				}));
+	private warnIfTriggerIsProblematic(value: string, trigger: Setting, component: TextComponent) {
+		const triggerStartsWithProblematicCharacter = this.problematicSymbols.some(problem => value.startsWith(problem));
+		const triggerIsOnlyWhitespace = value.length > 0 && value.trim().length === 0;
+		const triggerIsProblematic = triggerStartsWithProblematicCharacter || triggerIsOnlyWhitespace;
+		if (triggerIsProblematic) {
+			trigger.controlEl.addClass('setting-warning')
+			component.inputEl.setCustomValidity(`Using '${value}' as the trigger for inserting links might not work as intended`)
+			component.inputEl.reportValidity()
+		}
+	}
+
+	private removeValidationWarning(component: TextComponent, trigger: Setting) {
+		component.inputEl.setCustomValidity('')
+		component.inputEl.reportValidity()
+		trigger.controlEl.removeClass('setting-warning')
 	}
 }
