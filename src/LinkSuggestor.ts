cbf2014355616c7b6b3833f473123ad12633061d
Simon Clement <simon@simontc.com>
2022-05-21 20:14:23 +0300

    Move link suggestor to own file

---
+++ src/LinkSuggestor.ts
@@ -0,0 +1,115 @@
+import {
+	App,
+	Editor,
+	EditorPosition,
+	EditorSuggest,
+	EditorSuggestContext,
+	EditorSuggestTriggerInfo,
+	TFile
+} from "obsidian"
+import {SuggestionCollector} from "./SuggestionCollector"
+import {NoteCreationPreparer} from "./NoteCreationPreparer"
+import {ObsidianInterop} from "./ObsidianInterop"
+import {NoteAutoCreatorSettings} from "./NoteAutoCreatorSettings"
+import {extractSuggestionTrigger} from "./suggestionExtraction"
+import {Suggestion} from "./Suggestion"
+
+export class LinkSuggestor extends EditorSuggest<string> {
+	private readonly suggestionsCollector: SuggestionCollector
+	private readonly noteCreationPreparer: NoteCreationPreparer
+	private readonly obsidianInterop: ObsidianInterop
+	private currentTrigger: EditorSuggestTriggerInfo
+	private settings: NoteAutoCreatorSettings
+
+	constructor(app: App, settings: NoteAutoCreatorSettings) {
+		super(app)
+		this.obsidianInterop = new ObsidianInterop(app)
+		this.suggestionsCollector = new SuggestionCollector(this.obsidianInterop)
+		this.noteCreationPreparer = new NoteCreationPreparer(this.obsidianInterop)
+		this.settings = settings
+
+		const instructions = [
+			{command: 'Type |', purpose: 'to change display text'},
+		]
+		this.setInstructions(instructions)
+
+		// @ts-ignore
+		this.scope.register([], 'Tab', (event) => {
+			// @ts-ignore
+			// Undocumented field
+			const suggestionCollection = this.suggestions
+			const idOfHighlightedSuggestion = suggestionCollection.selectedItem
+			const highlightedSuggestion = suggestionCollection.values[idOfHighlightedSuggestion]
+			this.updateSuggestionLine(highlightedSuggestion)
+		})
+	}
+
+	getSuggestions(context: EditorSuggestContext): string[] | Promise<string[]> {
+		return this.suggestionsCollector.getSuggestions(context.query).map(s => s.Trigger)
+	}
+
+	onTrigger(cursor: EditorPosition, editor: Editor, file: TFile): EditorSuggestTriggerInfo | null {
+		const line = editor.getLine(cursor.line)
+		this.currentTrigger = extractSuggestionTrigger(line, {...cursor}, this.settings.triggerSymbol)
+		return this.currentTrigger
+	}
+
+	renderSuggestion(value: string, el: HTMLElement): void {
+		const suggestion = new Suggestion(value)
+
+		const triggerDiv = el.createDiv({
+			cls: "nac-suggestion-trigger",
+			text: suggestion.Trigger,
+		})
+		triggerDiv.hidden = true
+
+		el.createDiv({
+			cls: "suggestion-content",
+			text: suggestion.Title
+		})
+		el.createDiv({
+			cls: "suggestion-note",
+			text: suggestion.FolderPath + '/'
+		})
+	}
+
+	selectSuggestion(value: string, evt: MouseEvent | KeyboardEvent) {
+		const currentFile = this.context.file
+		this.selectSuggestionAsync(value, currentFile)
+	}
+
+	private async selectSuggestionAsync(suggestionString: string, currentFile: TFile) {
+		const suggestion = new Suggestion(suggestionString)
+
+		if (suggestion.Title === "") {
+			return
+		}
+
+		const creationCommand = this.noteCreationPreparer.prepareNoteCreationFor(suggestion)
+		const linkedFile = await this.obsidianInterop.getOrCreateFileAndFoldersInPath(creationCommand, suggestion, currentFile)
+		const linkToInsert = app.fileManager.generateMarkdownLink(linkedFile, currentFile.path, undefined, creationCommand.Alias)
+		this.replaceSuggestionWithLink(linkToInsert)
+		this.currentTrigger = undefined
+	}
+
+	private replaceSuggestionWithLink(valueToInsert: string) {
+		const editor = this.context.editor
+		const startPosition = {
+			line: this.currentTrigger.start.line,
+			ch: this.currentTrigger.start.ch - this.settings.triggerSymbol.length
+		}
+		editor.replaceRange(valueToInsert, startPosition, this.currentTrigger.end)
+	}
+
+	updateSuggestionLine(text: string) {
+		const editor = this.context.editor
+		const newSuggestion = new Suggestion(text)
+		const textToInsert = newSuggestion.VaultPathWithoutExtension
+		const finalCursorPosition = {
+			line: this.currentTrigger.start.line,
+			ch: this.currentTrigger.start.ch + textToInsert.length
+		}
+		editor.replaceRange(textToInsert, this.currentTrigger.start, this.currentTrigger.end)
+		editor.setCursor(finalCursorPosition)
+	}
+}
--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,6 @@
-import {
-	App,
-	Editor,
-	EditorPosition,
-	EditorSuggest,
-	EditorSuggestContext,
-	EditorSuggestTriggerInfo,
-	Plugin,
-	PluginSettingTab,
-	Setting,
-	TextComponent,
-	TFile
-} from 'obsidian'
-
-import {Suggestion} from "./Suggestion"
-import {SuggestionCollector} from "./SuggestionCollector"
-import {extractSuggestionTrigger} from "./suggestionExtraction"
-import {NoteCreationPreparer} from "./NoteCreationPreparer"
-import {ObsidianInterop} from "./ObsidianInterop"
+import {App, Plugin, PluginSettingTab, Setting, TextComponent} from 'obsidian'
 import {DEFAULT_SETTINGS, NoteAutoCreatorSettings} from "./NoteAutoCreatorSettings"
+import {LinkSuggestor} from "./LinkSuggestor"
 
 export default class NoteAutoCreator extends Plugin {
 	settings: NoteAutoCreatorSettings
@@ -41,100 +24,6 @@ export default class NoteAutoCreator extends Plugin {
 
 	async saveSettings() {
 		await this.saveData(this.settings)
-	}
-}
-
-class LinkSuggestor extends EditorSuggest<string>{
-	private readonly suggestionsCollector: SuggestionCollector
-	private readonly noteCreationPreparer: NoteCreationPreparer
-	private readonly obsidianInterop: ObsidianInterop
-	private currentTrigger: EditorSuggestTriggerInfo
-	private settings: NoteAutoCreatorSettings
-
-	constructor( app: App, settings: NoteAutoCreatorSettings ) {
-		super( app )
-		this.obsidianInterop = new ObsidianInterop(app)
-		this.suggestionsCollector = new SuggestionCollector(this.obsidianInterop)
-		this.noteCreationPreparer = new NoteCreationPreparer(this.obsidianInterop)
-		this.settings = settings
-
-		const instructions = [
-			{command: 'Type |', purpose: 'to change display text'},
-		]
-		this.setInstructions(instructions)
-
-		// @ts-ignore
-		this.scope.register([], 'Tab', (event) => {
-			// @ts-ignore
-			// Undocumented field
-			const suggestionCollection = this.suggestions
-			const idOfHighlightedSuggestion = suggestionCollection.selectedItem
-			const highlightedSuggestion = suggestionCollection.values[idOfHighlightedSuggestion]
-			this.updateSuggestionLine(highlightedSuggestion)
-		})
-	}
-
-	getSuggestions(context: EditorSuggestContext): string[] | Promise<string[]> {
-		return this.suggestionsCollector.getSuggestions(context.query).map(s => s.Trigger)
-	}
-
-	onTrigger(cursor: EditorPosition, editor: Editor, file: TFile): EditorSuggestTriggerInfo | null {
-		const line = editor.getLine( cursor.line )
-		this.currentTrigger = extractSuggestionTrigger(line, {...cursor}, this.settings.triggerSymbol)
-		return this.currentTrigger
-	}
-
-	renderSuggestion(value: string, el: HTMLElement): void {
-		const suggestion = new Suggestion(value)
-
-		const triggerDiv = el.createDiv({
-			cls: "nac-suggestion-trigger",
-			text: suggestion.Trigger,
-		})
-		triggerDiv.hidden = true
-
-		el.createDiv({
-			cls: "suggestion-content",
-			text: suggestion.Title
-		})
-		el.createDiv({
-			cls: "suggestion-note",
-			text: suggestion.FolderPath + '/'
-		})
-	}
-
-	selectSuggestion(value: string, evt: MouseEvent | KeyboardEvent) {
-		const currentFile = this.context.file
-		this.selectSuggestionAsync(value, currentFile)
-	}
-
-	private async selectSuggestionAsync(suggestionString: string, currentFile: TFile) {
-		const suggestion = new Suggestion(suggestionString)
-
-		if (suggestion.Title === "") {
-			return
-		}
-
-		const creationCommand = this.noteCreationPreparer.prepareNoteCreationFor(suggestion)
-		const linkedFile = await this.obsidianInterop.getOrCreateFileAndFoldersInPath(creationCommand, suggestion, currentFile)
-		const linkToInsert = app.fileManager.generateMarkdownLink(linkedFile, currentFile.path, undefined, creationCommand.Alias)
-		this.replaceSuggestionWithLink(linkToInsert)
-		this.currentTrigger = undefined
-	}
-
-	private replaceSuggestionWithLink(valueToInsert: string) {
-		const editor = this.context.editor
-		const startPosition = {line: this.currentTrigger.start.line, ch: this.currentTrigger.start.ch - this.settings.triggerSymbol.length}
-		editor.replaceRange(valueToInsert, startPosition, this.currentTrigger.end)
-	}
-
-	updateSuggestionLine(text: string) {
-		const editor = this.context.editor
-		const newSuggestion = new Suggestion(text)
-		const textToInsert = newSuggestion.VaultPathWithoutExtension
-		const finalCursorPosition = {line: this.currentTrigger.start.line, ch: this.currentTrigger.start.ch + textToInsert.length}
-		editor.replaceRange(textToInsert, this.currentTrigger.start, this.currentTrigger.end)
-		editor.setCursor(finalCursorPosition)
 	}
 }
 
