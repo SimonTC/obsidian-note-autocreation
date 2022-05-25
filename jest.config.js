5784a00bd6b6425b06a464e75508f8a07571bcd9
Simon Clement <simon@simontc.com>
2022-05-26 02:17:42 +0300

    Add jest-extended

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,7 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   silent: true,
+  setupFilesAfterEnv: ["jest-extended/all"],
   transform: {
     '^.+\\.ts?$': 'ts-jest',
   },
