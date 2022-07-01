17cdeedf18e683e8e5f3812935e71bc1254f6240
Simon Clement <simon@simontc.com>
2022-07-01 21:32:09 +0300

    Add initial test skeleton

---
+++ tests/headerSuggestionCollector.test.ts
@@ -0,0 +1,26 @@
+import {ExistingNoteSuggestion} from "../src/core/suggestions/NoteSuggestion"
+import 'jest-extended'
+
+const fakeNote = new ExistingNoteSuggestion('my note')
+
+test('no suggestions are returned when the note does not exist', () => {
+
+})
+
+test('no suggestions are returned when there are no headers in the note', () => {
+
+})
+
+describe('when there are headers in the note', function () {
+	test('all suggestions for all headers are returned when the query is empty', () => {
+
+	})
+
+	test('suggestions are filtered when the query is not empty', () => {
+
+	})
+
+	test('no suggestions are returned if no headers match the query', () => {
+
+	})
+})
