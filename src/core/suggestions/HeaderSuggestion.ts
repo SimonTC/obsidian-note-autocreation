4dfe4376fbfaabe598ba1a2ddea523c6ad46e2cc
Simon Clement <simon@simontc.com>
2022-07-01 22:20:42 +0300

    Add implementation for HeaderSuggestion

--- src/core/suggestions/HeaderSuggestion.ts
+++ src/core/suggestions/HeaderSuggestion.ts
@@ -1,14 +1,31 @@
 import {ISuggestion} from "./ISuggestion"
 
 export class HeaderSuggestion implements ISuggestion{
+	private readonly level: number
+	private readonly header: string
+
+	constructor(header: string, level: number) {
+		this.header = header
+		this.level = level
+	}
+
 	get Title(): string {
-		return ""
+		return this.header
 	}
 
 	render(el: HTMLElement): void {
+		const content = el.createDiv({
+			cls: "suggestion-content",
+			text: this.Title
+		})
+
+		content.createSpan({
+			cls: "suggestion-flair",
+			text: `H${this.level}`
+		})
 	}
 
 	get textToInsertOnLineUpdate(): string {
-		return ""
+		return this.header
 	}
 }
