56d0a135ff5ceeb5bd25acf28064f7ae07599651
Simon Clement <simon@simontc.com>
2022-08-14 08:54:47 +0300

    Make sure ObsidianFolderPath objects are created on settings load

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import {Plugin} from 'obsidian'
 import {DEFAULT_SETTINGS, NoteAutoCreatorSettings} from "./settings/NoteAutoCreatorSettings"
 import {SettingTab} from "./settings/SettingTab"
 import {LinkSuggestorInterop} from "./interop/LinkSuggestorInterop"
+import {ObsidianFolderPath} from "./core/paths/ObsidianFolderPath"
 
 export default class NoteAutoCreator extends Plugin {
 	settings: NoteAutoCreatorSettings
@@ -21,6 +22,9 @@ export default class NoteAutoCreator extends Plugin {
 
 	async loadSettings() {
 		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData())
+
+		// Recreating the relative top folders is necessary to make the paths are created as ObsidianFolderPath objects and not just anonymous objects.
+		this.settings.relativeTopFolders = this.settings.relativeTopFolders.map(f => new ObsidianFolderPath(f.VaultPath))
 	}
 
 	async saveSettings() {
