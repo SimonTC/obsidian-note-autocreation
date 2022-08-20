ce9a9bec050445a4aa2993d62ec5f5e196c748c1
Simon Clement <simon@simontc.com>
2022-08-20 10:44:30 +0300

    Show folder with final folder delimiter

--- src/settings/FolderSuggester.ts
+++ src/settings/FolderSuggester.ts
@@ -10,23 +10,35 @@ export class FolderSuggest extends TextInputSuggest<TFolder> {
         const lowerCaseInputStr = inputStr.toLowerCase()
 
         abstractFiles.forEach((folder: TAbstractFile) => {
-            if (
-                folder instanceof TFolder &&
-                folder.path.toLowerCase().contains(lowerCaseInputStr)
-            ) {
-                folders.push(folder)
-            }
+			if (folder instanceof TFolder) {
+				const lowerCaseFolderPath = folder.path.toLowerCase()
+				const queryCouldBeForThisFolder = () => lowerCaseFolderPath.contains(lowerCaseInputStr)
+				const queryIsAlsoForThisFolder = () => lowerCaseInputStr.endsWith('/') && `${lowerCaseFolderPath}/` === lowerCaseInputStr // Obsidian paths do not include '/' as the last symbol. We make this check to make sure that a query ending in '/' doesn't exclude the folder that would match that.
+				if (queryCouldBeForThisFolder() || queryIsAlsoForThisFolder()){
+					folders.push(folder)
+				}
+			}
         })
 
         return folders.sort((f1, f2) => f1.path.localeCompare(f2.path) )
     }
 
-    renderSuggestion(file: TFolder, el: HTMLElement): void {
-        el.setText(file.path)
+    renderSuggestion(folder: TFolder, el: HTMLElement): void {
+        const folderPath = folder.path
+		const pathToShow = this.addFinalFolderDelimiterToFolderNameIfNecessary(folderPath)
+		el.setText(pathToShow)
     }
 
-    selectSuggestion(file: TFolder): void {
-        this.inputEl.value = file.path
+	private addFinalFolderDelimiterToFolderNameIfNecessary(folderPath: string) {
+		return folderPath.endsWith('/')
+			? folderPath
+			: `${folderPath}/`
+	}
+
+	selectSuggestion(folder: TFolder): void {
+		const folderPath = folder.path
+		const pathToUse = this.addFinalFolderDelimiterToFolderNameIfNecessary(folderPath)
+        this.inputEl.value = pathToUse
         this.inputEl.trigger("input")
         this.close()
     }
