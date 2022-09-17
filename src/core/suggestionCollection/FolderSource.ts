069935bc301662a82e4db5ec5fd2a4f4195428c3
Simon Clement <simon@simontc.com>
2022-09-17 15:53:17 +0300

    Add sources

---
+++ src/core/suggestionCollection/FolderSource.ts
@@ -0,0 +1,23 @@
+import {IFileSystem} from "../../interop/ObsidianInterfaces"
+import {FolderSuggestion} from "../suggestions/FolderSuggestion"
+import {FolderQuery} from "../queries/FolderQuery"
+import {ObsidianFolderPath} from "../paths/ObsidianFolderPath"
+import {ISuggestionSource} from "./ISuggestionSource"
+
+export class FolderSource implements ISuggestionSource{
+	private readonly fileSystem: IFileSystem
+
+	constructor(fileSystem: IFileSystem) {
+		this.fileSystem = fileSystem
+	}
+
+	createSuggestionFromQuery(query: FolderQuery, existingSuggestionForQuery: FolderSuggestion): FolderSuggestion{
+		return existingSuggestionForQuery
+			? existingSuggestionForQuery
+			: new FolderSuggestion(new ObsidianFolderPath(query.query))
+	}
+
+	getAllPossibleSuggestions(query: FolderQuery): FolderSuggestion[]{
+		return this.fileSystem.getPathsToAllLoadedFolders().map(FolderSuggestion.FromPath)
+	}
+}
---
+++ src/core/suggestionCollection/NoteSource.ts
@@ -0,0 +1,70 @@
+import {
+	AliasNoteSuggestion,
+	ExistingNoteSuggestion,
+	NewNoteSuggestion,
+	NoteSuggestion
+} from "../suggestions/NoteSuggestion"
+import {IMetadataCollection} from "../../interop/ObsidianInterfaces"
+import {NoteAutoCreatorSettings} from "../../settings/NoteAutoCreatorSettings"
+import {FileQuery} from "../queries/FileQuery"
+import {ISuggestionSource} from "./ISuggestionSource"
+
+export class NoteSource implements ISuggestionSource{
+	private metadata: IMetadataCollection
+	private settings: NoteAutoCreatorSettings
+
+	constructor(metadata: IMetadataCollection, settings: NoteAutoCreatorSettings) {
+		this.metadata = metadata
+		this.settings = settings
+	}
+
+	createSuggestionFromQuery(query: FileQuery, existingSuggestionForQuery: NoteSuggestion): NoteSuggestion {
+		const queryAsSuggestion = new NewNoteSuggestion(query.query)
+		if (existingSuggestionForQuery){
+			const suggestionToAdd = queryAsSuggestion.HasAlias
+				? new ExistingNoteSuggestion(`${existingSuggestionForQuery.VaultPath}|${queryAsSuggestion.Alias}`)
+				: existingSuggestionForQuery
+
+			return suggestionToAdd
+		} else{
+			return queryAsSuggestion
+		}
+	}
+
+	getAllPossibleSuggestions(query: FileQuery): NoteSuggestion[]{
+		const observedPaths = new Set<string>()
+		const suggestions: NoteSuggestion[] = []
+		const trigger = query.query
+		const addIfPathHasNotBeSeen = (path: string, exist: boolean, alias: string | unknown) => {
+			const pathHasBeenSeen = observedPaths.has(path)
+			if (alias || !pathHasBeenSeen){
+				suggestions.push(this.createSuggestion(path, alias, exist, trigger))
+				observedPaths.add(path)
+			}
+			return !pathHasBeenSeen
+		}
+
+		const linkSuggestions = this.metadata.getLinkSuggestions()
+		for (const linkSuggestion of linkSuggestions) {
+			addIfPathHasNotBeSeen(linkSuggestion.path, linkSuggestion.file !== null, linkSuggestion.alias)
+		}
+
+		if(this.settings.suggestLinksToNonExistingNotes){
+			return suggestions
+		} else{
+			return suggestions.filter(suggestion => suggestion.ForExistingNote)
+		}
+	}
+
+	private createSuggestion(path: string, alias: string | unknown, pathIsToExistingNote: boolean, trigger: string): NoteSuggestion{
+		if(pathIsToExistingNote){
+			if (alias && (alias as string).toLowerCase().includes(trigger)){
+				return new AliasNoteSuggestion(path, alias as string)
+			} else {
+				return new ExistingNoteSuggestion(path)
+			}
+		} else {
+			return new NewNoteSuggestion(path)
+		}
+	}
+}
